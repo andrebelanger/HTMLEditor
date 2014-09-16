5e4a600a76924d8ff385529942cbf4d5cf9bdf40
Xcelled <j12@ketelaar.com>
2014-09-16 19:00:22 -0400

    Added a (very basic) buffer object

--- HtmlEditor/Buffer.cs
+++ HtmlEditor/Buffer.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace HtmlEditor
 {
-    public class Buffer
+	/// <summary>
+	/// A memory-backed text buffer
+	/// </summary>
+    public sealed class Buffer
     {
+		/// <summary>
+		/// The lines of text in the buffer
+		/// </summary>
+		/// <remarks>
+		/// In the interest of efficiency, it's probably not wise to make individual
+		/// character modifications on a string, so whatever consumes this object
+		/// should try to make changes to a single line all at once
+		/// </remarks>
+		public IList<string> Lines { get; private set; }
 
+		public Buffer()
+		{
+			Lines = new List<string>();
+		}
     }
 }
