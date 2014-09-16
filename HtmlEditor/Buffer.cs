4d0326f55960cd8d063dab503ee4099f469cbf0b
Xcelled <j12@ketelaar.com>
2014-09-16 19:27:18 -0400

    Fleshed the buffer class out some

--- HtmlEditor/Buffer.cs
+++ HtmlEditor/Buffer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,52 @@ namespace HtmlEditor
 		/// </remarks>
 		public IList<string> Lines { get; private set; }
 
+		/// <summary>
+		/// Initializes a new empty instance of the <see cref="Buffer"/> class.
+		/// </summary>
 		public Buffer()
 		{
 			Lines = new List<string>();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Buffer"/> class from the given lines.
+		/// </summary>
+		/// <param name="lines">The initial lines.</param>
+		public Buffer(IEnumerable<string> lines)
+		{
+			Lines = new List<string>(lines);
+		}
+
+		/// <summary>
+		/// Saves the lines in this buffer to the given <see cref="filename"/>.
+		/// 
+		/// If the file already exists, it is overwritten.
+		/// </summary>
+		/// <remarks>
+		/// Uses UTF8 encoding and the default platform line ending.
+		/// 
+		/// It is up to the caller to verify existing files etc
+		/// </remarks>
+		/// <param name="filename">The file to save to.</param>
+		public void Save(string filename)
+		{
+			File.WriteAllLines(filename, Lines);
+		}
+
+		/// <summary>
+		/// Creates a new buffer from the contents of the specified file.
+		/// </summary>
+		/// <remarks>
+		/// Uses UTF8 and the dfault platform line ending.
+		/// 
+		/// It is up to the caller to verify files exist etc.
+		/// </remarks>
+		/// <param name="filename">The file to load</param>
+		/// <returns>The constructed buffer.</returns>
+		public static Buffer Load(string filename)
+		{
+			return new Buffer(File.ReadAllLines(filename));
 		}
     }
 }
