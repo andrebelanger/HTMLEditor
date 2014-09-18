35d42ae65002374291b02f8cc8fb27d45fabf54c
Andre Belanger <andrebelanger@me.com>
2014-09-18 17:43:05 -0400

    Added HTMLSyntaxHighlighter, which highlights tags blue. Pretty messy, possibly to be re-factored later?

---
+++ HtmlEditor/SyntaxHighlighters/HTMLSyntaxHighlighter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace HtmlEditor.SyntaxHighlighters
+{
+    /// <summary>
+    /// HTML Formatter - for now, highlights tags
+    /// </summary>
+    public class HTMLSyntaxHighlighter: ISyntaxHighlighter
+    {
+        private static readonly SolidColorBrush BlueBrush = new SolidColorBrush(Colors.Blue);
+
+        public Inline Highlight(string text)
+        {
+            if (text.Length == 0)
+                return new Run();
+
+            var ret = new Span(); // Container for all of our text
+
+            bool elementFlag = false;
+
+            // TODO:: Clean this up to make it a bit more efficient?
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '<')
+                    elementFlag = true;
+                if (text[i] == '>')
+                {
+                    elementFlag = false;
+                    ret.Inlines.Add(new Span(new Run(text[i].ToString()) { Foreground = BlueBrush }));
+                    break;
+                }
+                    
+
+                if (elementFlag)
+                    ret.Inlines.Add(new Span(new Run(text[i].ToString()) { Foreground = BlueBrush }));
+                else
+                    ret.Inlines.Add(new Run(text[i].ToString()));
+
+            }
+                return ret;
+        }
+    }
+}
