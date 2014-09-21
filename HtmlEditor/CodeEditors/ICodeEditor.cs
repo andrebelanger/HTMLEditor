719f1ebc130d1bca5f6c55f997f0a57538a22a5c
Xcelled <j12@ketelaar.com>
2014-09-20 23:02:24 -0400

    Added basic CodeEditor interface

---
+++ HtmlEditor/CodeEditors/ICodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlEditor.CodeEditors
+{
+	/// <summary>
+	/// Defines basic operations supported by all code editors
+	/// </summary>
+	public interface ICodeEditor
+	{
+		/// <summary>
+		/// Loads the specified lines into the editor.
+		/// </summary>
+		/// <param name="lines">The lines.</param>
+		void Load(IEnumerable<string> lines);
+
+		/// <summary>
+		/// Returns the current lines in the editor
+		/// </summary>
+		/// <returns>The current lines</returns>
+		IEnumerable<string> Save();
+	}
+}
