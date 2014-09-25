82774195ca885e5d6fc44f0da288e38a22719944
Xcelled <j12@ketelaar.com>
2014-09-25 18:25:36 -0400

    Tweaked html heirarchy

--- HtmlEditor/Parser/HtmlElement.cs
+++ HtmlEditor/Parser/HtmlElement.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HtmlEditor.Parser
 {
+	[DebuggerDisplay("<{Tag}>")]
     public class HtmlElement : HtmlObject
     {
-        public string TagType { get; set; }
-	    public string Tag { get; set; }
-        public ICollection<HtmlObject> Children { get; set; }
-
-        public HtmlElement Parent { get; set; }
+		public string Tag { get;  set; }
+		public ICollection<HtmlObject> Children { get; private set; }
+		public Dictionary<string, string> Attributes { get; private set; }
 
 	    public HtmlElement()
 	    {
 		    Children = new List<HtmlObject>();
+		    Attributes = new Dictionary<string, string>();
 	    }
     }
 }
